ad0cdf1b32c338c26e2da75a7bf2aaaf568d5e37
JlKol1 <yulyak2603@gmail.com>
2021-03-01 20:01:45 +0300

    task js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+   if(n > 0){
+    return +n.toString().split('').reverse().join('')
+   }else{
+  let res = -1 * n;
+ return +res.toString().split('').reverse().join('')
+   }
+
   
 }
